cff145a8d358a190fe9929dc0cca107bb0bcb4b4
Timothy Putra Prasetio <timothypp02@gmail.com>
2023-12-19 10:23:25 +0700

    Use debouncing in search

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,7 +1,7 @@
 'use client';
+import { useDebounce, useDebouncedCallback } from 'use-debounce';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { SearchParamsContext } from 'next/dist/shared/lib/hooks-client-context.shared-runtime';
 import { useSearchParams, useRouter, usePathname } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
@@ -9,10 +9,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
-    console.log(term);
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching for... ${term}`);
+
     const params = new URLSearchParams(searchParams);
-    
     if (term) {
       params.set('query', term);
     } else {
@@ -20,7 +20,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
 
     replace(`${pathname}?${params.toString()}`)
-  }
+  }, 300)
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
